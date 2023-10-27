18dd1f4e19631347fa24b5be17e4fe85350a9468
vcaunegre <vincent.caunegre@gmail.com>
2023-10-27 08:52:49 +0200

    for production

--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -2,9 +2,18 @@ package org.example;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Utils {
-    private static Dotenv dotenv = Dotenv.load();
-   public static String PG_URL = dotenv.get("PG_URL");
-   public static String PG_PASSWORD =dotenv.get("PG_PASSWORD");
-   public static String PG_USER = dotenv.get("PG_USER");
+
+        private static Dotenv dotenv = inProduction() ? null : Dotenv.load();
+        public static String PG_URL = inProduction() ? System.getenv("PG_URL") : dotenv.get("PG_URL") ;
+        public static String PG_PASSWORD = inProduction() ? System.getenv("PG_PASSWORD") : dotenv.get("PG_PASSWORD") ;
+        public static String PG_USER =  inProduction() ? System.getenv("PG_USER") : dotenv.get("PG_USER") ;
+
+    public static boolean inProduction(){
+        String port=System.getenv("PORT");
+        return port != null;
+    }
 }
